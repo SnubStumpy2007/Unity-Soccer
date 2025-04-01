ae4c726135ded427909bfed9c865c5fa5cd5b468
SnubStumpy2007 <joshua192007@gmail.com>
2025-03-31 20:17:49 -0400

    Updates
    Corrected code, so enemy balls bounce away from players instead of the other way around.

--- Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -57,15 +57,15 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer =  transform.position + other.gameObject.transform.position; 
+            Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position; 
            
             if (hasPowerup) // if have powerup hit enemy with powerup force
             {
-                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+                enemyRigidbody.AddForce(-awayFromPlayer * powerupStrength, ForceMode.Impulse);
             }
             else // if no powerup, hit enemy with normal strength 
             {
-                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+                enemyRigidbody.AddForce(-awayFromPlayer * normalStrength, ForceMode.Impulse);
             }
 
 
